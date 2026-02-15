310eeccd9d475afc483082ecb7eeac7f28851dc7
savaseeratesli <savaseeratesli@gmail.com>
2026-02-15 19:43:19 +0300

    entity class

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c186383e25b4b2264495e8142e82d2ac9cb368ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83868369fe58effbe3cebcba853ab7f67a6d73d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
