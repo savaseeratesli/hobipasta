b43adb2ee0869dd194b20657c9d2769a34b71255
savaseeratesli <savaseeratesli@gmail.com>
2026-02-15 20:53:16 +0300

    seed data

--- e-commerce/API/Data/DataContext.cs
+++ e-commerce/API/Data/DataContext.cs
@@ -7,5 +7,18 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products => Set<Product>();
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product>
+            {
+                new Product { Id=1, Name="Çikolatalı Pasta", Description="Bitter", ImageUrl="1.jpg", Price=1500, IsActive=true, Stock=3 },
+                new Product { Id=2, Name="Beyaz Çikolatalı Pasta", Description="Vanilya", ImageUrl="2.jpg", Price=1000, IsActive=true, Stock=2 },
+                new Product { Id=3, Name="Çilekli Pasta", Description="Çilek", ImageUrl="3.jpg", Price=2000, IsActive=true, Stock=4 },
+                new Product { Id=4, Name="Muzlu Pasta", Description="Muz", ImageUrl="4.jpg", Price=1800, IsActive=true, Stock=1 },           
+            }
+        );
+    }
 }
