1c779b072f3841a1a43ad5be5ddee7a08a21546a
savaseeratesli <savaseeratesli@gmail.com>
2026-02-16 13:36:16 +0300

    interface iproduct

--- e-commerce/Client/src/App.tsx
+++ e-commerce/Client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { IProduct } from "./model/IProduct";
 
 function App() {
   return (
@@ -17,11 +18,7 @@ function Header() {
 
 function ProductList() {
 
-  const [products, setProducts] = useState([
-      { id:1, name: "product 1", price: 1000, isActive: true },
-      { id:2, name: "product 2", price: 2000, isActive: false },
-      { id:3, name: "product 3", price: 3000, isActive: true },
-  ]);
+  const [products, setProducts] = useState<IProduct[]>([]);
 
   useEffect(() => {
       fetch("http://localhost:5047/api/products")
@@ -30,8 +27,14 @@ function ProductList() {
   }, []);
 
   function addProduct(){
-    setProducts([...products, { id:Date.now(), name: "product 4", price: 4000, isActive: true }])
-  }
+    setProducts([...products, 
+      { 
+        id:Date.now(), 
+        name: "product 4", 
+        price: 4000, 
+        isActive: true 
+      }])
+    }
 
   console.log("render...");
 
