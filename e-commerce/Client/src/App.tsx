2708338dff971e479d434ea521074041730bb5ed
savaseeratesli <savaseeratesli@gmail.com>
2026-02-16 11:01:18 +0300

    react comp

--- e-commerce/Client/src/App.tsx
+++ e-commerce/Client/src/App.tsx
@@ -1,9 +1,37 @@
 function App() {
 
   return (
-    <h1>React.js</h1>
-
+    <>
+    <Header />
+    <ProductList />
+    </>
   )
+}
+
+function Header(){
+  return (
+    <h1>Header</h1>
+  );
+}
+
+function ProductList(){
+  return (
+    <div>
+      <h2>ProductList</h2>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+function Product(){
+  return(
+    <>
+      <h3>Product</h3>
+    </>    
+  );
 }
 
 export default App
