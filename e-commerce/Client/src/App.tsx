9957283a270b6fee8e7804f1d4caf5a467bdd6d7
savaseeratesli <savaseeratesli@gmail.com>
2026-02-16 11:16:42 +0300

    jsx

--- e-commerce/Client/src/App.tsx
+++ e-commerce/Client/src/App.tsx
@@ -5,38 +5,37 @@ const products = [
 ];
 
 function App() {
-
   return (
     <>
-    <Header />
-    <ProductList />
+     <Header />
+     <ProductList />
     </>
   )
 }
 
-function Header(){
+function Header() {
   return (
     <h1>Header</h1>
   );
 }
 
-function ProductList(){
+function ProductList() {
   return (
     <div>
       <h2>ProductList</h2>
-      <Product product={products[0]} />
-      <Product product={products[1]} />
-      <Product product={products[2]} />
+      <Product product={products[0]}/>
+      <Product product={products[1]}/>
+      <Product product={products[2]}/>
     </div>
   );
 }
 
-function Product(props: any){
-  return(
+function Product(props: any) {
+  return (
     <div>
-      <h3>{ props.products.name }</h3> 
-      <p>{ props.products.price }</p> 
-    </div>    
+      <h3>{ props.product.name }</h3>
+      <p> { props.product.price }</p>
+    </div>
   );
 }
 
