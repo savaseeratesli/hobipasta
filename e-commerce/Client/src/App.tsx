c2ec91d5c00491a9a1efc7372376b2c09fea67d8
savaseeratesli <savaseeratesli@gmail.com>
2026-02-16 11:13:10 +0300

    reacj jsx

--- e-commerce/Client/src/App.tsx
+++ e-commerce/Client/src/App.tsx
@@ -1,3 +1,9 @@
+const products = [
+  { name: "product 1", price: 1000 },
+  { name: "product 2", price: 2000 },
+  { name: "product 3", price: 3000 },
+];
+
 function App() {
 
   return (
@@ -18,19 +24,19 @@ function ProductList(){
   return (
     <div>
       <h2>ProductList</h2>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product product={products[0]} />
+      <Product product={products[1]} />
+      <Product product={products[2]} />
     </div>
   );
 }
 
-function Product(){
+function Product(props: any){
   return(
-    <>
-      <h3>Product</h3>
-    </>    
+    <div>
+      <h3>{ props.products.name }</h3> 
+      <p>{ props.products.price }</p> 
+    </div>    
   );
 }
 
