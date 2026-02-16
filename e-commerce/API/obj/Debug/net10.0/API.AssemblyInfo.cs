49fd52e57a9477930e50fd01768f32d43f5c60a8
savaseeratesli <savaseeratesli@gmail.com>
2026-02-16 15:55:13 +0300

    card tasatımı

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7336e76b45c67ae6c954af4addf30f9ec3650299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e99a77e3bbd10b0bc029fdb2bc678faa15514f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
