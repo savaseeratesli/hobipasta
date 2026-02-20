08654a2bfc6d5929b714413a35968524d1ce208c
savaseeratesli <savaseeratesli@gmail.com>
2026-02-20 15:24:13 +0300

    cart controller

---
+++ e-commerce/API/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+
+public class CartController: ControllerBase
+{
+    private readonly DataContext _context;
+    public CartController(DataContext context)
+    {
+        _context = context;
+    }
+
+    //endpoint
+    [HttpGet]
+    public async Task<ActionResult<Cart>> GetCart()
+    {
+        var cart = await _context.Carts
+                    .Include(i => i.CartItems)
+                    .ThenInclude(i => i.Product)
+                    .Where(i => i.CustomerId == Request.Cookies["customerId"])
+                    .FirstOrDefaultAsync();
+
+        if(cart == null) 
+        {
+            return NotFound();
+        }
+
+        return cart;
+    }
+}
