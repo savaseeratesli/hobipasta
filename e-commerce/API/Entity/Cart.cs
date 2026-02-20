4cc385bd3da897e5f552b155c7cb98decd4e4e39
savaseeratesli <savaseeratesli@gmail.com>
2026-02-20 14:58:56 +0300

    cart object methods

--- e-commerce/API/Entity/Cart.cs
+++ e-commerce/API/Entity/Cart.cs
@@ -6,6 +6,33 @@ public class Cart
     public int CartId { get; set; }
     public string CustomerId { get; set; } = null!; 
     public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public void AddItem(Product product, int quantity)
+    {
+        var item = CartItems.Where(c => c.ProductId == product.Id).FirstOrDefault();
+        
+        if(item == null)
+        {
+            CartItems.Add(new CartItem{ Product = product, Quantity = quantity});
+        }
+        else
+        {
+            item.Quantity += quantity;
+        }
+    }
+    public void DeleteItem(int productId, int quantity)
+    {
+        var item = CartItems.Where(c => c.ProductId == productId).FirstOrDefault();
+
+        if(item == null) return;
+
+        item.Quantity -= quantity;
+
+        if(item.Quantity == 0)
+        {
+            CartItems.Remove(item);
+        }
+    }
 }
 
 public class CartItem
