9026d8ae0e2262ee6ebb76ece6b44f7983a970f9
savaseeratesli <savaseeratesli@gmail.com>
2026-02-20 13:58:18 +0300

    cart migration

---
+++ e-commerce/API/Entity/Cart.cs
@@ -0,0 +1,19 @@
+namespace API.Entity;
+
+//Herhangi bir kiiş sepetine ürün eklediğinde cart sınıfında  bir nesne üretilecek.
+public class Cart
+{
+    public int CartId { get; set; }
+    public string CustomerId { get; set; } = null!; 
+    public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+}
+
+public class CartItem
+{
+    public int CartItemId { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+    public int CartId { get; set; }
+    public Cart Cart { get; set; } = null!;
+    public int Quantit { get; set; } 
+}
--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27639ae27096e7be0354f3c3ee705ec1c24a942a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16346656060d804339c34b12d0c890241613362a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
