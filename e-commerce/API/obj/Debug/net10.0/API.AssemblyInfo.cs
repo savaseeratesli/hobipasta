2aa2307d68fec0a7d18151b7e8b5e850d4afa089
savaseeratesli <savaseeratesli@gmail.com>
2026-02-22 22:33:00 +0300

    cart test

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3089c77a29eed15c2e750e6b41142027d2732b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317c6d0c386a9a084a2e81b843942d1c37720b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
