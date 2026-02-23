8bc12c39613590da43adb611713d49b8850db5ca
savaseeratesli <savaseeratesli@gmail.com>
2026-02-23 10:39:01 +0300

    get cart in app start sepet adet güncelleme

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa2307d68fec0a7d18151b7e8b5e850d4afa089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc23c2cb6a47964c271030a197f7efc04fb2e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- e-commerce/Client/src/components/Header.tsx
+++ e-commerce/Client/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, Button, IconButton, Stack, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router";
+import { useCartContext } from "../context/CartContext";
 
 const links = [
   { title: "Home", to: "/"},
@@ -22,6 +23,9 @@ const navStyles = {
 }
 
 export default function Header() {
+    const { cart } = useCartContext();
+    const itemCount = cart?.cartItems.reduce((total, item) => total + item.quantity, 0)
+
     return (
       <AppBar position="static" sx={{ mb: 4 }}>
         <Toolbar sx={ { display: "flex", justifyContent: "space-between"} }>
@@ -38,7 +42,7 @@ export default function Header() {
 
             <Box sx={{ display: "flex", alignItems: "center"}}>
                 <IconButton component={Link} to="/cart" size="large" edge="start" color="inherit">
-                  <Badge badgeContent="2" color="secondary">
+                  <Badge badgeContent={itemCount} color="secondary">
                     <ShoppingCart/>
                   </Badge>
                 </IconButton>
