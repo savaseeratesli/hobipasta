50a4ffe65afabe72b80e7046e6c1c27837b93571
savaseeratesli <savaseeratesli@gmail.com>
2026-02-26 00:13:41 +0300

    adapter caching

---
+++ e-commerce/Client/src/features/catalog/catalogSlice.ts
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import { IProduct } from "../../model/IProduct";
+import requests from "../../api/requests";
+
+export const fetchProducts = createAsyncThunk<IProduct[]>(
+    "catalog/fetchProducts",
+    async () => {
+        return await requests.Catalog.list();
+    }
+)
+
+export const fetchProductById = createAsyncThunk<IProduct, number>(
+    "catalog/fetchProductById",
+    async (productId) => {
+        return await requests.Catalog.details(productId);
+    }
+)
+
+const productsAdapter = createEntityAdapter<IProduct>();
+
+const initialState = productsAdapter.getInitialState({
+    status: "idle"
+});
+
+export const catalogSlice = createSlice({
+    name: "catalog",
+    initialState,
+    reducers: {},
+    extraReducers: (builder => {
+        builder.addCase(fetchProducts.pending, (state) =>{
+            state.status = "pendingFetchProducts";
+        });
+        builder.addCase(fetchProducts.fulfilled, (state, action) => {
+            productsAdapter.setAll(state, action.payload);
+            state.status = "idle";
+        });
+        builder.addCase(fetchProducts.rejected, (state) =>{
+            state.status = "idle";
+        });
+
+        builder.addCase(fetchProductById.pending, (state) =>{
+            state.status = "pendingFetchProductById";
+        });
+        builder.addCase(fetchProductById.fulfilled, (state, action) => {
+            productsAdapter.upsertOne(state, action.payload);
+            state.status = "idle";
+        });
+        builder.addCase(fetchProductById.rejected, (state) =>{
+            state.status = "idle";
+        });
+    })
+})
--- e-commerce/Client/src/store/store.ts
+++ e-commerce/Client/src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { counterSlice } from "../features/counter/counterSlice"
 import { cartSlice } from "../features/cart/cartSlice"
+import { catalogSlice } from "../features/catalog/catalogSlice"
 
 export const store = configureStore(
     {
         reducer: {
             counter: counterSlice.reducer,
-            cart: cartSlice.reducer
+            cart: cartSlice.reducer,
+            catalog: catalogSlice.reducer
         }
         
     })
