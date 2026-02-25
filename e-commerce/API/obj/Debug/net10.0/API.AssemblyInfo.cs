ba13395e91b57fc7215a20b409b8bf2ecb68df83
savaseeratesli <savaseeratesli@gmail.com>
2026-02-25 11:16:27 +0300

    cartslice testler

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb934288632647a8ba962a49e4bd5cf323f1a9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21936b5176299c4109c61d958065f31a3fcae002")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
