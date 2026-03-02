d016912b74a6e262454106e628cd9db58fbaca2f
savaseeratesli <savaseeratesli@gmail.com>
2026-03-02 15:44:04 +0300

    scalar testleri

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25787489ed31e10cd484d873542a575e592a84a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94fd838b4c2006f10e032701d3bb2586221a626")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
