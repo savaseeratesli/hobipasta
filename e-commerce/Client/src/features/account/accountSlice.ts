28a0f2886ed7fb3af4261cef7c6f34aeea142a1e
savaseeratesli <savaseeratesli@gmail.com>
2026-03-03 23:37:06 +0300

    logout test

--- e-commerce/Client/src/features/account/accountSlice.ts
+++ e-commerce/Client/src/features/account/accountSlice.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { User } from "../../model/IUser";
 import { FieldValues } from "react-hook-form";
 import requests from "../../api/requests";
+import { router } from "../../router/Routes";
 
 interface AccountState {
     user: User | null;
@@ -30,7 +31,13 @@ export const loginUser = createAsyncThunk<User, FieldValues>(
 export const accountSlice = createSlice({
     name: "account",
     initialState,
-    reducers: {},
+    reducers: {
+        logout: (state) => {
+            state.user = null;
+            localStorage.removeItem("user");
+            router.navigate("/catalog");
+        }
+    },
     extraReducers: (builder => {
         builder.addCase(loginUser.fulfilled, (state, action) => {
             state.user = action.payload;
@@ -38,3 +45,5 @@ export const accountSlice = createSlice({
     })
 })
 
+//logoutu dışarı açıyorum
+export const { logout } = accountSlice.actions;
--- e-commerce/Client/src/layout/Header.tsx
+++ e-commerce/Client/src/layout/Header.tsx
@@ -1,7 +1,8 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, Button, IconButton, Stack, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router";
-import { useAppSelector } from "../hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { logout } from "../features/account/accountSlice";
 
 const links = [
   { title: "Home", to: "/"},
@@ -29,6 +30,8 @@ const navStyles = {
 
 export default function Header() {
     const { cart } = useAppSelector(state => state.cart);
+    const { user } = useAppSelector(state => state.account);
+    const dispatch = useAppDispatch();
     const itemCount = cart?.cartItems.reduce((total, item) => total + item.quantity, 0)
 
     return (
@@ -51,13 +54,21 @@ export default function Header() {
                     <ShoppingCart/>
                   </Badge>
                 </IconButton>
-                <Stack direction="row">
-                { authLinks.map(link => 
-                  <Button key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>
-                ) }
-              </Stack>
+                {
+                  user ? (
+                    <Stack direction="row">
+                      <Button sx={navStyles}>{user.name}</Button>
+                      <Button sx={navStyles} onClick={() => dispatch(logout())}>Çıkış</Button>
+                    </Stack>
+                  ):(
+                    <Stack direction="row">
+                        { authLinks.map(link => 
+                        <Button key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>
+                      ) }
+                    </Stack>
+                  )
+                } 
             </Box>
-
         </Toolbar>
       </AppBar>
     );
