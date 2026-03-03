b55c8eb67f1f2eb12e1eb6dcae8d539fda02e45b
savaseeratesli <savaseeratesli@gmail.com>
2026-03-03 23:39:55 +0300

    testler

--- e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ e-commerce/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0322f22d20e1f74a091337f50d3e9344324afe86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a0f2886ed7fb3af4261cef7c6f34aeea142a1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
