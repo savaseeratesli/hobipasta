11eca8d85520e573c78bfa7f86c9d9cc4ad0a4bf
savaseeratesli <savaseeratesli@gmail.com>
2026-03-15 20:32:15 +0300

    stepper satış adımları tasarlandı.

--- e-commerce/Client/src/features/checkout/CheckOutPage.tsx
+++ e-commerce/Client/src/features/checkout/CheckOutPage.tsx
@@ -1,21 +1,86 @@
-import { Info } from "@mui/icons-material";
-import { Grid, Paper } from "@mui/material";
+import { Box, Button, Grid, Paper, Step, StepLabel, Stepper } from "@mui/material";
+import Info from "./Info";
 import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import Review from "./Review";
+import { useState } from "react";
+import { ChevronLeftRounded, ChevronRightRounded } from "@mui/icons-material";
 
-export default function CheckOutPage()
+const steps = ["Teslimat Bilgileri","Ödeme","Sipariş Özeti"];
+
+function getStepContent(step: number)
 {
+    switch(step) {
+        case 0:
+            return <AddressForm />;
+        case 1:
+            return <PaymentForm />;
+        case 2:
+            return <Review />;
+        default:
+            throw new Error("Bilinmeyen bir Adım.");
+    }
+}
+
+export default function CheckoutPage()
+{
+    const [activeStep, setActiveStep] = useState(0);
+
+    function handleNext() {
+        setActiveStep(activeStep + 1);
+    }
+
+    function handlePrevious() {
+        setActiveStep(activeStep - 1);
+    }
+
     return (
         <Paper>
-            <Grid container sx={{p:4}} spacing={2}>
+            <Grid container sx={{p:4}} spacing={5}>
                 <Grid size={4}>
                     <Info />
                 </Grid>
                 <Grid size={8}>
-                    <AddressForm />
-                    <PaymentForm />
-                    <Review />
+                   <Box >
+                        <Stepper activeStep={activeStep} sx={{height: 40}}>
+                            { steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                   </Box>
+                   <Box>
+                        {activeStep === steps.length ? (
+                            <h2>Siparişiniz Alındı.</h2>
+                        ) : (
+                            <>
+                                {getStepContent(activeStep)}
+                                <Box>
+
+                                    <Box sx={
+                                        [
+                                            {
+                                                display: "flex",
+                                            },
+                                            activeStep !== 0 
+                                                ? { justifyContent: "space-between" }
+                                                : { justifyContent: "flex-end" }
+                                        ]
+                                    }>
+                                        {
+                                            activeStep !== 0 && 
+                                                <Button startIcon={<ChevronLeftRounded />} variant="contained" 
+                                                onClick={handlePrevious}>Geri</Button>
+                                        }
+
+                                        <Button startIcon={<ChevronRightRounded />} variant="contained" 
+                                            onClick={handleNext}>İleri</Button>
+                                    </Box>
+                                </Box>
+                            </>
+                        )}           
+                   </Box>
                 </Grid>
             </Grid>
         </Paper>
